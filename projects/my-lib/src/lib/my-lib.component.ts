25650fb858c3c5b0d0331efc86c7adfc18d57665
Max Gomes <maxgomes92@live.com>
2020-10-07 23:27:40 +0200

    Add sip.js

--- projects/my-lib/src/lib/my-lib.component.ts
+++ projects/my-lib/src/lib/my-lib.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MyLibService } from './my-lib.service';
+import { SimpleUser, SimpleUserOptions } from "sip.js/lib/platform/web";
 
 @Component({
   selector: 'lib-my-lib',
@@ -7,12 +8,32 @@ import { MyLibService } from './my-lib.service';
 })
 export class MyLibComponent implements OnInit {
   public currentTime: string;
+  public simpleUser: SimpleUser;
 
   constructor(
     public myLibService: MyLibService,
-  ) { }
+  ) {
+
+  }
 
   ngOnInit(): void {
+    const server = "wss://sip.example.com";
+    const aor = "sip:alice@example.com";
+    const options: SimpleUserOptions = {
+      aor,
+      media: {
+        local: {
+          video: (document.getElementById('localVideo') as HTMLVideoElement)
+        },
+        remote: {
+          video: (document.getElementById('remoteVideo') as HTMLVideoElement)
+        }
+      }
+    };
+
+    this.simpleUser = new SimpleUser(server, options);
+
+    console.log('isConnected', this.simpleUser.isConnected())
   }
 
   onClick() {
